8c2947a585bf3a5b0b3d77e69e76040c44168c6a
Sparkpin <nipkraps@gmail.com>
2018-11-22 22:24:29 -0600

    appbase: Put AppBase in blackcombBase, too

--- include/blackcomb/base/AppBase.h
+++ include/blackcomb/base/AppBase.h
@@ -6,22 +6,24 @@
 #include <GLFW/glfw3.h>
 #include "Window.h"
 
-/**
- * A convenient base for any Blackcomb application.
- */
-class AppBase {
-public:
-    AppBase(std::string winTitle = blackcombBase::DefaultWinName);
-    ~AppBase();
-    blackcombBase::Window& getWindow();
-    virtual void run();
-    virtual void update();
-    virtual void processInput();
-    float deltaTime = 0.0f;
-    float lastFrame = static_cast<float>(glfwGetTime());
-private:
-    std::optional<blackcombBase::Window> window;
-};
+namespace blackcombBase {
+    /**
+     * A convenient base for any Blackcomb application.
+     */
+    class AppBase {
+    public:
+        AppBase(std::string winTitle = blackcombBase::DefaultWinName);
+        ~AppBase();
+        blackcombBase::Window& getWindow();
+        virtual void run();
+        virtual void update();
+        virtual void processInput();
+        float deltaTime = 0.0f;
+        float lastFrame = static_cast<float>(glfwGetTime());
+    private:
+        std::optional<blackcombBase::Window> window;
+    };
+}
 
 
 #endif //CRAFTCLONE_APPBASE_H
--- src/base/AppBase.cpp
+++ src/base/AppBase.cpp
@@ -4,67 +4,69 @@
 #include "blackcomb/misc/EngineException.h"
 #include "blackcomb/base/AppBase.h"
 
-AppBase::AppBase(std::string winTitle) {
-    stbi_set_flip_vertically_on_load(true);
-    glfwInit();
+namespace blackcombBase {
+    AppBase::AppBase(std::string winTitle) {
+        stbi_set_flip_vertically_on_load(true);
+        glfwInit();
 
-    getWindow().hide();
-    getWindow().setWindowTitle(winTitle);
-    getWindow().makeContextCurrent();
-    getWindow().setInputMode(GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        getWindow().hide();
+        getWindow().setWindowTitle(winTitle);
+        getWindow().makeContextCurrent();
+        getWindow().setInputMode(GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        throw EngineException("Failed to initialize GLAD");
+        if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+            throw EngineException("Failed to initialize GLAD");
+        }
+
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        getWindow().show();
     }
 
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-    getWindow().show();
-}
-
-AppBase::~AppBase() {
-    glfwTerminate();
-}
-
-/**
- * A getter for the window, since the window is lazily generated.
- */
-blackcombBase::Window& AppBase::getWindow() {
-    if (!window) {
-        window = blackcombBase::Window();
-    }
-    return window.value();
-}
-
-/**
- * Call this function to run your application.
- * Starts a loop that every frame updates delta time, handles input, clears colour and depth bits, calls update(), swaps the buffers, and polls for events.
- * Can be overriden for fine grained control, but it is recommended you override AppBase.update instead.
- */
-void AppBase::run() {
-    while (!getWindow().shouldClose()) {
-        float now = static_cast<float>(glfwGetTime());
-        deltaTime = now - lastFrame;
-        lastFrame = now;
-        processInput();
-        getWindow().mouseWatcher.update(getWindow());
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        update();
-
-        glfwSwapBuffers(getWindow().getHandle());
-        glfwPollEvents();
+    AppBase::~AppBase() {
+        glfwTerminate();
     }
 
+    /**
+     * A getter for the window, since the window is lazily generated.
+     */
+    blackcombBase::Window& AppBase::getWindow() {
+        if (!window) {
+            window = blackcombBase::Window();
+        }
+        return window.value();
+    }
+
+    /**
+     * Call this function to run your application.
+     * Starts a loop that every frame updates delta time, handles input, clears colour and depth bits, calls update(), swaps the buffers, and polls for events.
+     * Can be overriden for fine grained control, but it is recommended you override AppBase.update instead.
+     */
+    void AppBase::run() {
+        while (!getWindow().shouldClose()) {
+            float now = static_cast<float>(glfwGetTime());
+            deltaTime = now - lastFrame;
+            lastFrame = now;
+            processInput();
+            getWindow().mouseWatcher.update(getWindow());
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            update();
+
+            glfwSwapBuffers(getWindow().getHandle());
+            glfwPollEvents();
+        }
+
+    }
+
+    /**
+     * Called once per frame after run() is called.
+     */
+    void AppBase::update() {}
+
+    /**
+     * Input handling code. Run once per frame after run() is called.
+     */
+    void AppBase::processInput() {}
 }
-
-/**
- * Called once per frame after run() is called.
- */
-void AppBase::update() {}
-
-/**
- * Input handling code. Run once per frame after run() is called.
- */
-void AppBase::processInput() {}
